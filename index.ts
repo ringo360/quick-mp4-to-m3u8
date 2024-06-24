bd1b073df3fcd290fc3e2cc0ac0418aec9f5a299
ringoXD <nep360i@gmail.com>
2024-06-24 20:38:12 +0900

    uwu

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
+import { spawn } from 'child_process';
 import consola from 'consola';
 
 const version = '1.0.0';
 
 consola.info(`Quick MTM v${version}`);
+
+async function main() {
+	const file = await consola.prompt('Type target file name:', {
+		placeholder: 'input.mp4',
+	});
+	consola.info(file);
+	const file_x = file.replace(/\.[^/.]+$/, '');
+	//todo: 自動でディレクトリ作成
+	const run_cmd = `ffmpeg -i target/${file} -c:v copy -c:a copy -f hls -hls_time 9 -hls_playlist_type vod -hls_segment_filename "out/output-${file_x}-%5d.ts" out/output-${file_x}.m3u8`;
+	spawn(run_cmd, [], { shell: true, stdio: 'inherit' });
+}
+
+main();
