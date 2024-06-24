8fe71bb183b4248606ec2ffde25114cd58090992
ringoXD <nep360i@gmail.com>
2024-06-24 22:57:05 +0900

    complete

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import { spawn } from 'child_process';
 import consola from 'consola';
+import { promises as fs } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const version = '1.0.0';
-
 consola.info(`Quick MTM v${version}`);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 async function main() {
 	const file = await consola.prompt('Type target file name:', {
@@ -12,7 +17,9 @@ async function main() {
 	consola.info(file);
 	const file_x = file.replace(/\.[^/.]+$/, '');
 	//todo: 自動でディレクトリ作成
-	const run_cmd = `ffmpeg -i target/${file} -c:v copy -c:a copy -f hls -hls_time 9 -hls_playlist_type vod -hls_segment_filename "out/output-${file_x}-%5d.ts" out/output-${file_x}.m3u8`;
+	const outDir = join(__dirname, './out/' + file_x);
+	await fs.mkdir(outDir, { recursive: true });
+	const run_cmd = `ffmpeg -i target/${file} -c:v copy -c:a copy -f hls -hls_time 9 -hls_playlist_type vod -hls_segment_filename "${outDir}/output-${file_x}-%5d.ts" ${outDir}/output-${file_x}.m3u8`;
 	spawn(run_cmd, [], { shell: true, stdio: 'inherit' });
 }
 
